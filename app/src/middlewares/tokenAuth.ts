1212b92f0b0130582a12ba957c7876a19b29cfed
Ricardo Perez <sanexto@gmail.com>
2021-09-28 17:44:13 -0300

    Cambio de funciones en la validacion.

--- app/src/middlewares/tokenAuth.ts
+++ app/src/middlewares/tokenAuth.ts
@@ -54,7 +54,7 @@ function tokenAuth(profile: UserBase.Profile): (req: Request, res: Response, nex
   
       } catch(_) {}
   
-      if (payload != null && 'id' in payload && validator.isInt(payload.id.toString(), { min: config.types.id.min, max: config.types.id.max, allow_leading_zeroes: false })) {
+      if (!_.isNull(payload) && _.has(payload, 'id') && validator.isInt(payload.id.toString(), { min: config.types.id.min, max: config.types.id.max, allow_leading_zeroes: false })) {
 
         payload.id = parseInt(payload.id.toString());
 
